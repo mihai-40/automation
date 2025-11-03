ce020bbc3c30f027278f3c3e44467cd561bdaa6a
mihai-40 <basescu_a@yahoo.com>
2025-11-03 16:32:06 +0200

    Second github Commit

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Second commit");
         System.out.println("First Github commit");
+        System.out.println("Second Github commit");
     }
 
 
