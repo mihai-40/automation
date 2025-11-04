afc78a9209e116de7b2c28777deabfb4863017f7
mihai-40 <basescu_a@yahoo.com>
2025-11-04 13:31:09 +0200

    cleaner

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,10 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import test.java.HomePage;
 
 
-import java.sql.Driver;
 
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
