b48001b2ae04f223bda88943efd1547fb53b932c
mihai-40 <basescu_a@yahoo.com>
2025-11-06 17:04:51 +0200

    working on accepting cookies

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,33 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import test.java.HomePage;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+
+import java.sql.Driver;
+import java.time.Duration;
+
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-@Test
+
+
+
 public class App {
+
+
     public static void main(String[] args) {
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.emag.ro/");
 
         HomePage homepage = new HomePage(driver);
         assertTrue(homepage.checkWebsite());
+        assertTrue(homepage.acceptCookies());
+
 
 
     }
--- src/main/java/test/java/HomePage.java
+++ src/main/java/test/java/HomePage.java
@@ -1,8 +1,14 @@
 package test.java;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class HomePage {
 
@@ -14,17 +20,36 @@ public class HomePage {
         this.driver = driver;
     }
 
-    By Logo = By.xpath("//*[@id='masthead']/div/div/div[1]/a/img");
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    By logo = By.xpath("//*[@id='masthead']/div/div/div[1]/a/img");
+    By acceptCookies = By.xpath("//*[text()='Accept toate']");
+   // By AddButton = By.xpath();
 
     //functie care verifica ca suntem pe emag
 
     public boolean checkWebsite() {
 
-       boolean result = driver.findElement(Logo).isDisplayed();
+       boolean result = driver.findElement(logo).isDisplayed();
        if(result) {System.out.print("element was found");}
        return result;
 
     }
+
+    //public boolean checkAddToBasket(){}
+
+    public boolean acceptCookies(){
+
+       driver.findElement(acceptCookies).click();
+       return true;
+
+
+    }
+
+    public By checkAcceptedCookies(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(acceptCookies));
+        return acceptCookies;
+    }
+
 }
 
 
